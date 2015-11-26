f4a110000e7155f82415d000026a50b050f6d5f0
Parham Alvani <parham.alvani@gmail.com>
2015-11-26 15:38:53 +0330

    Current source codes were added.

---
+++ atmel/atm16/atm16/main.c
@@ -0,0 +1,27 @@
+/*
+ * atm16.c
+ *
+ * Created: 11/26/2015 12:54:04 AM
+ * Author : Parham Alvani
+ */ 
+
+#include <avr/io.h>
+#include <stdint.h>
+#include <util/delay.h>
+
+int main(void)
+{
+    /* Replace with your application code */
+    DDRC = 0x00;
+	while (1) 
+    {
+		PORTA = 0xff;
+		_delay_ms(500);
+		char c = 0x00;
+		c |= PINC;
+		_delay_ms(2000);
+		PORTA = c;
+		_delay_ms(10000);
+	}
+}
+
