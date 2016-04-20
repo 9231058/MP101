b7aca402ca79a573a3f23eb2e346cff31123bb74
Parham Alvani <parham.alvani@gmail.com>
2016-04-20 18:04:21 +0430

    Another sample of wave was added :)

--- src/wave2/wave2/main.asm
+++ src/wave2/wave2/main.asm
@@ -9,37 +9,38 @@
 .org $000
 reset_label:
 	jmp reset_isr
-.org $012
-tov0_label:
-	jmp tov0_isr 
+.org $026
+tcp0_label:
+	jmp tcp0_isr 
 reset_isr:
 	cli
 	ldi r16 , LOW(RAMEND) 
 	out SPL , r16 
 	ldi r16 , HIGH(RAMEND)
 	out SPH , r16
-	; Enabling timer counter 0 overflow interrupt
-	ldi r16, (1<<TOIE0)
+	; Enabling timer counter 0 compare match interrupt
+	ldi r16, (1<<OCIE0)
 	out TIMSK, r16
 	; PB3 --> output
 	ldi r16, (1<<PB3)
 	out DDRB, r16
 	; Enabling timer counter 0 with 1024 prescaling
+	; CTC Mode
 	ldi r16, $ff
 	out OCR0, r16
-	ldi r16, (0<<CS02) | (1<<CS01) | (1<<CS00) | (1<<WGM01) | (1<<WGM00) | (1<<COM00) | (1<<COM01) 
+	ldi r16, (1<<CS02) | (1<<CS00) | (1<<WGM01) | (0<<WGM00) | (1<<COM00) | (0<<COM01) 
 	out TCCR0, r16
-tov0_isr:
+tcp0_isr:
 	cli
 	in r16, OCR0
 	cpi r16, 0
-	breq tov0_isr_reset
-tov0_isr_dec:
+	breq tcp0_isr_reset
+tcp0_isr_dec:
 	subi r16, $0f
-	jmp tov0_isr_store
-tov0_isr_reset:
+	jmp tcp0_isr_store
+tcp0_isr_reset:
 	ldi r16, $ff
-tov0_isr_store:
+tcp0_isr_store:
 	out OCR0, r16
 	sei
 start:
