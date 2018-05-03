96e13d453de1933ba0464b0a48192899ae368980
Parham Alvani <parham.alvani@gmail.com>
2018-05-03 15:37:49 +0430

    Corrects interrupt routine.

--- src/led/led/main.asm
+++ src/led/led/main.asm
@@ -6,39 +6,48 @@
 ;
 
 
-.org $000
+.org 0x0000
 reset_label:
-	jmp reset_isr
-.org $004
+	rjmp reset_isr
+
+.org 0x0004
 int1_label:
-	jmp int1_isr 
+	rjmp int1_isr 
+
 reset_isr:
 	cli
+
+	; Setup RAM
 	ldi r16 , LOW(RAMEND) 
 	out SPL , r16 
 	ldi r16 , HIGH(RAMEND)
 	out SPH , r16
+	
 	; PD0 --> output
 	ldi r16, (1<<PD0)
 	out DDRD, r16
+	
 	; Enable INT1 with low level trigger
 	; Enabling with GICR
 	in r16, GICR
 	ori r16, (1<<INT1)
 	out GICR, r16
+
 	; Triggering mode with MCUCR
 	in r16, MCUCR
 	andi r16, $F3
 	out MCUCR, r16
+	
 	sei
-	jmp start
+	
+	jmp loop
+
 int1_isr:
-	cli
 	in r16, PORTD
 	ldi r17, (1<<PD0)
 	eor r16, r17
 	out PORTD, r16
-	sei
-start:
-	jmp start
+	reti
 
+loop:
+	jmp loop
