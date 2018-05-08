5a40bf9cc5ef85330c68277dd12dfc08ab44128e
Parham Alvani <parham.alvani@gmail.com>
2018-05-08 23:05:00 +0430

    Adds ports example.

---
+++ src/ports/ports/main.asm
@@ -0,0 +1,26 @@
+;
+; ports.asm
+;
+; Created: 5/8/2018 10:10:56 PM
+; Author : parham
+;
+
+
+.org 0x0000
+jmp start
+
+start:
+	; tri-state input
+	ldi r16, (0 << PORTD0)
+	out PORTD, r16
+
+	nop
+
+	; high ouput
+	ldi r16, (1 << PORTD0)
+	out PORTD, r16
+	ldi r16, (1 << PD0)
+	out DDRD, r16
+
+loop:
+	rjmp loop    
