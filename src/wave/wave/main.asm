32bf46ff95406f568d1ec9988e9e82616d53bc12
Parham Alvani <parham.alvani@gmail.com>
2018-05-08 08:13:14 +0430

    Corrects fast pwm project.

--- src/wave/wave/main.asm
+++ src/wave/wave/main.asm
@@ -6,12 +6,13 @@
 ;
 
 
-.org $000
+.org $0000
 reset_label:
 	jmp reset_isr
-.org $012
+.org $0012
 tov0_label:
-	jmp tov0_isr 
+	jmp tov0_isr
+
 reset_isr:
 	cli
 	ldi r16 , LOW(RAMEND) 
@@ -21,16 +22,20 @@ reset_isr:
 	; Enabling timer counter 0 overflow interrupt
 	ldi r16, (1<<TOIE0)
 	out TIMSK, r16
-	; PB3 --> output
+	; PB3 (OC0) --> output
 	ldi r16, (1<<PB3)
 	out DDRB, r16
-	; Enabling timer counter 0 with 1024 prescaling
+	; Enabling timer counter 0 fast PWM with 256 prescaling
+	; OC0 inverting mode
 	ldi r16, $ff
 	out OCR0, r16
-	ldi r16, (0<<CS02) | (1<<CS01) | (1<<CS00) | (1<<WGM01) | (1<<WGM00) | (1<<COM00) | (1<<COM01) 
+	ldi r16, (1<<CS02) | (0<<CS01) | (0<<CS00) | (1<<WGM01) | (1<<WGM00) | (1<<COM00) | (1<<COM01) 
 	out TCCR0, r16
+
+	sei
+	jmp start
+
 tov0_isr:
-	cli
 	in r16, OCR0
 	cpi r16, 0
 	breq tov0_isr_reset
@@ -41,6 +46,7 @@ tov0_isr_reset:
 	ldi r16, $ff
 tov0_isr_store:
 	out OCR0, r16
-	sei
+	reti
+
 start:
-	jmp start
+	rjmp start
